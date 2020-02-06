d287dd675048e24a8856443558c26bc9f7366aee
Santiago <sanrinconr@correo.udistrital.edu.co>
2020-02-06 16:54:11 -0500

    Lista simple finalizada

--- listaSimple.cpp
+++ listaSimple.cpp
@@ -1,3 +1,9 @@
+/*
+*LISTA SIMPLE
+*Santiago Rincon Robelto - 20172020084
+*Josue Alexander Nuñez Prada - 20172020071
+*/
+
 #include "listaSimple.h"
 
 
@@ -5,9 +11,24 @@ using namespace std;
 
 int main(){
 	Lista<int> lis;
-	lis.insertar(3,0);
-	lis.insertar(9,1);
+
+	//POS 0
+	lis.insertarInicio(2);
+	//POS 1
+	lis.insertarFinal(3);
+	//POS 2
+	lis.insertarFinal(4);
+	//POS 3
+	lis.insertarFinal(5);
+	//POS 4
+	lis.insertarFinal(6);
+
+	lis.eliminar(0);
+	lis.insertar(52,2);
+
+	lis.imprimirLista();
 
 
+	//EN CASO DE QUE SE USE INSERTAR EN UNA POSICION NO EXISTENTE SE UTILIZARA insertarFinal
 	return 0;
 }
--- listaSimple.h
+++ listaSimple.h
@@ -4,123 +4,160 @@
 using namespace std;
 
 template <class T>
+
+//DECLARACION DE LA ESTRUCTURA
 struct nodo {
 	nodo<T> *sig;
 	T info;
 };
 
+//DECLARACION DE LA CLASE QUE CONTIENE UNA ESTRUCTURA TIPO NODO
 template <class T>
 class Lista{
 
-	nodo<T> *cab;
-	int tam;
+	nodo<T> *cab; //Un apuntador a un nodo
+	int tam; //un tamano o identificador
 
-	public:
-		Lista(){
-			tam = 0;
-		}
-		bool listaVacia();
-		int tamanoLista();
-		void insertar(T infoNueva, int pos);
-		void insertarFinal(T infoNueva);
-		void insertarInicio(T infoNueva);
-		void eliminar(int pos);
-		void imprimirLista();
-		~Lista(){
-			for(int i=1;i<=tam;i++){
-				nodo<T> *aux;
-				aux = cab;
-				cab =cab->sig;
-				delete aux;
-			}
-		}
+public:
+	Lista(){
+		cab = NULL; //Inicialmente no va a existir un nodo al cual apuntar
+		tam = 0; // Naturalmente el tamno inicial sera 0
+	}
+	bool listaVacia();
+	int tamanoLista();
+	void insertar(T infoNueva, int pos); //USADO SOLO PARA MODIFICAR EXISTENTES
+	void insertarFinal(T infoNueva); //USADO PARA AGREGAR NUEVOS
+	void insertarInicio(T infoNueva);//USADO PARA AGREGAR UNICAMENTE EL INICIAL
+	void eliminar(int pos);
+	void imprimirLista();
+
 };
 
+/*
+*Se determina si la lista tiene o no elementos
+*@return  1 si hay elementos
+*@return  0 si no hay elementos
+*/
 template <class T>
 bool Lista <T>::listaVacia(){
 	if (tam==0)
-		return true;
+	return true;
 	else
-		return false;
+	return false;
 }
 
+/*
+*Se determinan la cantidad de elementos de la lista
+*@return  int cantidad elementos
+*/
 template <class T>
-int Lista<T>::tamanoLista(){return tam;}
+int Lista<T>::tamanoLista(){
+	return tam;
+}
 
+/*
+*Se inserta el primer elemento a la lista
+*@param (T) infoNueva: el valor a guardar en el primer espacio
+*/
 template <class T>
 void Lista<T>::insertarInicio(T infoNueva){
-	nodo<T> *nuevo = new nodo <T>;
-	cab=nuevo;
+	nodo<T> *nuevo = new nodo <T>; //Se crea un apuntador que apunta a un nuevo nodo
+	nuevo -> sig = NULL; //Ese nuevo nodo no va a apuntar a ningun lado (cuando se agrege otro si que lo hara)
+	nuevo-> info =  infoNueva; //Se le pone la informacion al nuevo nodo
+	cab=nuevo; //Al apuntador cab que estaba en null ahora se le da un nodo al que apuntar
+	tam++; // Se incremena el tamano de la variable de la CLASE
+	//OTRA MANERA DE ESCRIBIRLO
+	// (*nuevo).sig =  NULL;
 }
 
+/*
+*Se inserta un elemento al final de la lista
+*@param (T) infoNueva: el valor a guardar en el ultimo espacio
+*/
 template <class T>
 void Lista<T>::insertarFinal(T infoNueva){
-	nodo<T> *nuevo = new nodo <T>, *aux;
-	nuevo->info=infoNueva;
+	nodo<T> *nuevo = new nodo <T>;
+	nodo<T> *aux ;
 	aux=cab;
-	for(int i=1;i<tam-1;i++){
+	nuevo->info=infoNueva;
+
+	for(int i=0;i<tam-1;i++){
 		aux=aux->sig;
 	}
-	nuevo->sig=aux->sig;
 	aux->sig=nuevo;
+	nuevo->sig=NULL;
+	tam++;
+
 }
 
+/*
+*Se inserta un nuevo elemento en un espacio ya existente
+*@param (T) infoNueva: el valor a guardar en el primer espacio
+*@param (T) pos: la posicion que se va a modificar (debe de existir)
+*/
 template <class T>
 void Lista<T>::insertar(T infoNueva, int pos){
-
-/*	if(pos>tam){
-		std::cout << "Coefwe" << '\n';
-		insertarFinal(infoNueva);
-	//AQUI ENTRARA UNICAMENTE CON EL 0 SI SE LE PASA COMO POSICION 1 ENTRA AL IF DE ARRIBA Y DA ERROR
-	}else if(pos<=1){
-		insertarInicio(infoNueva);*/
-
-/*Corregido*/
-/* Se debe definir si la lista empieza por 0 o 1, en caso de que empiece po 0 queda asi*/
-/* En todo caso es mejor invertir los if en caso de que se quiera que la lista empiece por 1*/
-
 	if(pos==0){
-		std::cout << "Coefwe" << '\n';
 		insertarInicio(infoNueva);
-	}else if(pos>tam){
+	}else if(pos>=tam){
 		insertarFinal(infoNueva);
+
 	}else{
-		nodo<T> *nuevo = new nodo <T>, *aux;
-		nuevo->info=infoNueva;
-		aux=cab;
-		for(int i=1;i<pos-1;i++){
+		nodo<T> *modificado; //Se crea el el nuevo nodo
+		nodo<T> *aux ;
+		aux=cab; //El apuntador aux se iguala con el apuntador que apunta a la cabeza de la lista
+		for(int i=1;i<pos-1;i++){ //Se hace avanzar aux desde la cabeza hasta pos-1 nodo existente
 			aux=aux->sig;
 		}
-		nuevo->sig=aux->sig;
-		aux->sig=nuevo;
+		modificado = aux->sig;
+		modificado->info = infoNueva;
 		tam++;
 	}
 }
 
+/*
+*Se elimina el nodo de cierta posicion
+*@param (T) pos: la posicion que se va a eliminar (debe de existir)
+*/
 template <class T>
 void Lista<T>::eliminar(int pos){
+
 	nodo<T> *aux1, *aux2;
 	aux1=cab;
 	aux2=cab;
-	for(int i=1;i<pos-1;i++){
-		aux1=aux1->sig;
+	if(pos == 0){//Si se desea eliminar la posicion 1
+		aux1 = cab; //Se iguala aux 1 a el elemento 0
+		aux1 = aux1->sig; //Se avanza aux1 al elemento 1
+		cab = aux1; //La nueva cabeza sera el elemento 1
+		delete aux2; //Se elimina el elemento 0
+	}else{
+		for(int i=1;i<pos;i++){
+			aux1=aux1->sig;
+			aux2=aux2->sig;
+		}
+		aux1=aux1->sig; //AUX1 estara apuntando al nodo que se desea eliminar
+		aux2->sig=aux1->sig; //AUX2 apuntara al nuevo nodo (despues del eliminado) por medio del siguiente se aux1
+		delete aux1;
+		tam--;
 	}
-	for(int i=1;i<pos+1;i++){
-		aux2=aux2->sig;
-	}
-	aux1->sig=aux2->sig;
-	aux1=aux1->sig;
-	delete aux1;
 }
 
+/*
+*Se imprime la lista actual
+*/
 template <class T>
 void Lista<T>::imprimirLista(){
 	nodo<T> *aux;
 	aux = cab;
-	for(int i=1;i<=tam;i++){
+	while(aux->sig != NULL){
 		cout<<aux->info<<endl;
 		aux=aux->sig;
 	}
+	//Se imprime el ultimo elemento
+	if(aux->sig == NULL){
+		cout<<aux->info<<endl;
+	}
+
 }
 
 #endif
